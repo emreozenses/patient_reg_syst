2c9d0ad95f88d738bec4310e7aa313d507c308ad
Emre OZENSES <emreozenses@gmail.com>
2024-08-18 19:46:18 +0300

    DtoConverter completed

--- src/main/java/com/tiga/patient_reg_syst/converter/DtoConverter.java
+++ src/main/java/com/tiga/patient_reg_syst/converter/DtoConverter.java
@@ -30,11 +30,36 @@ public class DtoConverter {
         return new AddressResponse(address.getDescription());
     }
 
+    public static List<AddressResponse> convertToAddressResponseList (List<Address> addresses){
+        List<AddressResponse> addressResponseList = new ArrayList<>();
+        addresses.forEach(address -> {
+            addressResponseList.add(convertToAddressResponse(address));
+        });
+        return addressResponseList;
+    }
+
+
     public static PassportResponse convertToPassportResponse (Passport passport){
         return new PassportResponse(passport.getPassportNumber());
+    }
+
+    public static List<PassportResponse> convertToPassportResponseList (List<Passport> passports){
+        List<PassportResponse> passportResponseList = new ArrayList<>();
+        passports.forEach(passport -> {
+            passportResponseList.add(convertToPassportResponse(passport));
+        });
+        return passportResponseList;
     }
 
     public static TcknResponse convertToTcknResponse (Tckn tckn){
         return new TcknResponse(tckn.getTckn());
     }
+    public static List<TcknResponse> convertToTcknResponseList (List<Tckn> tckns){
+        List<TcknResponse> tcknResponseList = new ArrayList<>();
+        tckns.forEach(tckn -> {
+            tcknResponseList.add(convertToTcknResponse(tckn));
+        });
+        return tcknResponseList;
+    }
+
 }
