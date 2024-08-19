035fdd48722c2016544928babebef98ed96f6d15
Emre OZENSES <emreozenses@gmail.com>
2024-08-19 23:31:52 +0300

    securityConfig added

---
+++ src/main/java/com/tiga/patient_reg_syst/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.tiga.patient_reg_syst.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://cdpn.io"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.cors().configurationSource(corsConfigurationSource());
+        return httpSecurity.csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> {
+                    auth.requestMatchers("/welcome/**").permitAll();
+                    auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers("/actuator/**").permitAll();
+                    auth.requestMatchers(HttpMethod.GET,"/account/**")
+                            .hasAnyAuthority("ADMIN","USER");
+                    auth.requestMatchers(HttpMethod.POST,"/account/**")
+                            .hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.PUT,"/account/**")
+                            .hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE,"/account/**")
+                            .hasAuthority("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                //.oauth2Login(Customizer.withDefaults())
+                .build();
+
+    }
+
+
+
+}
+
