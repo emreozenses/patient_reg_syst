afda118750a1e47ca341663568c80929cd7903bb
Emre OZENSES <emreozenses@gmail.com>
2024-08-22 13:06:47 +0300

    filter criterias updated

--- src/main/java/com/tiga/patient_reg_syst/repository/PatientRepository.java
+++ src/main/java/com/tiga/patient_reg_syst/repository/PatientRepository.java
@@ -1,8 +1,6 @@
 package com.tiga.patient_reg_syst.repository;
 
-import com.tiga.patient_reg_syst.dto.PatientQueryResponse;
 import com.tiga.patient_reg_syst.entity.Patient;
-import com.tiga.patient_reg_syst.entity.PatientWithAge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,19 +8,21 @@ import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
-    @Query(value = "SELECT * FROM public.patients AS p WHERE p.first_name ILIKE %:name%",nativeQuery = true)
+    @Query(value = "SELECT * FROM public.patients AS p WHERE p.first_name ILIKE :name%",nativeQuery = true)
     List<Patient> findByName(String name);
 
-    @Query(value = "SELECT * FROM public.patients AS p WHERE p.surname ILIKE %:surname%",nativeQuery = true)
+    @Query(value = "SELECT * FROM public.patients AS p WHERE p.surname ILIKE :surname%",nativeQuery = true)
     List<Patient> findBySurname(String surname);
 
-    @Query(value = "SELECT * FROM public.patients AS p WHERE p.gender ILIKE %:gender% ORDER BY p.first_name ASC",nativeQuery = true)
+    @Query(value = "SELECT * FROM public.patients AS p WHERE p.gender ILIKE :gender% ORDER BY p.first_name ASC",nativeQuery = true)
     List<Patient> filterByGender(String gender);
     @Query(value = "SELECT * FROM public.patients AS p ORDER BY p.birth_date ASC ",nativeQuery = true)
     List<Patient> orderByBirthDateAsc();
 
-    @Query(value = "SELECT *, age(birth_date) AS age FROM public.patients",nativeQuery = true)
-    List<PatientWithAge> orderByAge();
+    @Query(value = "SELECT * FROM public.patients AS p ORDER BY p.age ASC",nativeQuery = true)
+    List<Patient> orderByAge();
+
+
 
 
 }
